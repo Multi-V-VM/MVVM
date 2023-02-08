52289bca3a06b30bf689422a597de6dc1cb6d9a9
victoryang00 <yangyiwei2000@gmail.com>
2023-02-07 17:19:38 -0800

    feat: stage to delete everything rust.

--- src/codegen/mod.rs
+++
@@ -1,2 +0,0 @@
-mod optimizer;
-mod regmap;
--- src/frontend/mod.rs
+++
@@ -1,9 +0,0 @@
-#[allow(unreachable_code)]
-pub mod binary;
-pub mod cache;
-pub mod elf;
-pub mod instruction;
-pub mod page;
-
-static mut BIT_LENGTH: i8 = 0;
-static mut IS_E: bool = false;
