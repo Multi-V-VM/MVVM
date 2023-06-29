c60029aa53967430d34c2805f174521f2291fee3
victoryang00 <yangyiwei2000@gmail.com>
2023-06-29 03:00:00 +0000

    add export files

--- include/wamr_export.h
+++ include/wamr_export.h
@@ -9,6 +9,12 @@ extern "C" {
 void insert_fd(int, char const *, int);
 void remove_fd(int);
 void serialize_to_file(struct WASMExecEnv*);
+void insert_socket(char const *, int);
+void remove_socket(char const *); // see whether there's socket maintainance impl in wasi?
+void insert_lock(char const *, int);
+void insert_sem(char const *, int);
+void remove_lock(char const *);
+void remove_sem(char const *);
 #ifdef __cplusplus
 }
 #endif
