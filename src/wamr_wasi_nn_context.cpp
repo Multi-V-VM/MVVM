14ecab17ecd2617ab10dc6997bc347eef1595887
Yiwei Yang <40686366+victoryang00@users.noreply.github.com>
2023-10-26 12:57:31 -0700

    add llamma

--- src/wamr_wasi_nn_context.cpp
+++ src/wamr_wasi_nn_context.cpp
@@ -1,8 +1,10 @@
 //
 // Created by victoryang00 on 10/25/2023.
 //
+#if WASM_ENABLE_WASI_NN != 0
 #include "wamr_wasi_nn_context.h"
 #include "wamr.h"
 extern WAMRInstance *wamr;
 void WAMRWASINNContext::dump_impl(WASINNContext *env) {}
 void WAMRWASINNContext::restore_impl(WASINNContext *env) {}
+#endif
